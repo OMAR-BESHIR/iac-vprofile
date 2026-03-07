d2290acaf668b1c1ff4f57b93fb9f816b8ea634a
Omar Reda Beshir <omarreda011284@gmail.com>
2026-03-07 16:31:04 +0200

    fixed versions

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.28"
+      version = "~> 6.28"
     }
 
     random = {
@@ -32,7 +32,7 @@ terraform {
     region = "us-east-1"
   }
 
-  required_version = ">= 1.6.3"
+  required_version = ">= 1.6"
 }
 ##
 ##
