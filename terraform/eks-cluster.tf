7619f65789f82a4f35a38b1158de8851ea349e95
Omar Reda Beshir <omarreda011284@gmail.com>
2026-03-07 16:24:46 +0200

    update cluster

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,38 +1,27 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 21.0"
+  version = "~> 20.0"
 
   cluster_name    = local.cluster_name
   cluster_version = "1.33"
 
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
+    instance_types = ["t3.small"]
   }
 
   eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
-
-      instance_types = ["t3.small"]
-
+    default = {
       min_size     = 1
       max_size     = 3
       desired_size = 2
-    }
-
-    two = {
-      name = "node-group-2"
 
       instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
+      capacity_type  = "ON_DEMAND"
     }
   }
 }
