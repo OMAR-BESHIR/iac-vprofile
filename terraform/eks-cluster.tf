635718cda30fb9f4682f9a87a5189acafee044de
Omar Reda Beshir <omarreda011284@gmail.com>
2026-03-11 00:40:49 +0200

    add some policies

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,27 +1,38 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
-
   cluster_name    = local.cluster_name
   cluster_version = "1.33"
-
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
-
   cluster_endpoint_public_access = true
-
   eks_managed_node_group_defaults = {
     instance_types = ["t3.small"]
   }
-
   eks_managed_node_groups = {
     default = {
       min_size     = 1
       max_size     = 3
       desired_size = 2
-
       instance_types = ["t3.small"]
       capacity_type  = "ON_DEMAND"
+    }
+  }
+ 
+  enable_cluster_creator_admin_permissions = true
+
+  access_entries = {
+    github_actions = {
+      principal_arn = "arn:aws:iam::532523613767:user/YOUR_IAM_USERNAME"
+      type          = "STANDARD"
+      policy_associations = {
+        admin = {
+          policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+          access_scope = {
+            type = "cluster"
+          }
+        }
+      }
     }
   }
 }
